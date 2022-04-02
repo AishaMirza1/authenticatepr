4f0e4c36e68363ef9bc4819826e7868444a5007e
unknown <aishamirza0002@gmail.com>
2022-04-02 13:47:17 +0530

    added environment var

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
@@ -18,7 +20,7 @@ const userSchema = new mongoose.Schema({
 });
 
 var secret = 'thisissecretkey';
-userSchema.plugin(encrypt, { secret:secret ,encryptedFields:['password'] });
+userSchema.plugin(encrypt, { secret:process.env.SECRET ,encryptedFields:['password'] });
 
 const User = mongoose.model('User',userSchema)
 
