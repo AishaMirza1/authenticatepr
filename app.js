5b3f317f8b635ad6d83a491596cbb140327fe08d
unknown <aishamirza0002@gmail.com>
2022-04-02 11:48:06 +0530

    added database encryption using npm mongoose-encryption

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const ejs = require('ejs')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const encrypt = require('mongoose-encryption')
+const app = express()
+
+
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static('public'))
+
+
+mongoose.connect('mongodb://localhost:27017/userDB')
+const userSchema = new mongoose.Schema({
+    email:String,
+    password: String
+});
+
+var secret = 'thisissecretkey';
+userSchema.plugin(encrypt, { secret:secret ,encryptedFields:['password'] });
+
+const User = mongoose.model('User',userSchema)
+
+app.get('/',(req,res)=>{
+    res.render(`home`)
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
+app.get('/register',(req,res)=>{
+    res.render('register')
+})
+
+app.post('/register',(req,res)=>{
+    const username = req.body.username
+    const password = req.body.password
+ 
+    const newUser = new User({
+        email:username,
+        password:password
+    });
+
+    newUser.save((err)=>{
+        if(!err){
+            res.render('secrets')
+        }else{
+            console.log(err)
+        }
+    })
+
+})
+
+app.post('/login',(req,res)=>{
+    const username = req.body.username
+    const password = req.body.password
+
+    User.findOne({email:username},(err,founduser)=>{
+        if(err){
+            console.log(err)
+        }else{
+            if(founduser){
+                if(founduser.password === password){
+                    res.send('/secrets')
+                }else{
+                    res.send('password not matched')
+                }
+            }else{
+                res.send('user not found')
+            }
+        }
+            
+    })
+})
+
+
+
+
+app.listen(3000,(req,res)=>{
+    console.log('server started at 3000')
+})
